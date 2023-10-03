7ce11dd3c88be7fb201d37557bfe64738e16153b
Tim <hi@timcreated.it>
2023-10-03 13:03:44 +0200

    feat: api setup (#3)

--- packages/figmage_api/lib/figmage_api.dart
+++
@@ -1,4 +0,0 @@
-/// The Figma API client for figmage
-library figmage_api;
-
-export 'src/figmage_api.dart';
--- packages/figmage_api/lib/src/figmage_api.dart
+++
@@ -1,7 +0,0 @@
-/// {@template figmage_api}
-/// The Figma API client for figmage
-/// {@endtemplate}
-class FigmageApi {
-  /// {@macro figmage_api}
-  const FigmageApi();
-}

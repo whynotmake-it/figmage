64f255bfea84d48a56b51825aa6eed2994b48a21
JesperBllnbm <31738080+JesperBllnbm@users.noreply.github.com>
2023-10-03 21:13:41 +0200

    cleanup

--- packages/figma_variables_api/lib/figma_variables_api.dart
+++ packages/figma_variables_api/lib/figma_variables_api.dart
@@ -1,4 +1,6 @@
 /// Figma REST API for variables.
 library figma_variables_api;
 
-export 'src/figma_variables_api.dart';
+export 'src/client.dart';
+export 'src/query.dart';
+export 'src/models.dart';
--- packages/figma_variables_api/lib/src/figma_variables_api.dart
+++
@@ -1,7 +0,0 @@
-/// {@template figma_variables_api}
-/// Figma REST API for variables.
-/// {@endtemplate}
-class FigmaVariablesApi {
-  /// {@macro figma_variables_api}
-  const FigmaVariablesApi();
-}

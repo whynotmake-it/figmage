f1edc1dd62ae35de00431f67235fa2e92b28574c
JesperBllnbm <31738080+JesperBllnbm@users.noreply.github.com>
2023-10-03 17:38:32 +0200

    create variables api package

---
+++ packages/figma_variables_api/lib/src/figma_variables_api.dart
@@ -0,0 +1,7 @@
+/// {@template figma_variables_api}
+/// Figma REST API for variables.
+/// {@endtemplate}
+class FigmaVariablesApi {
+  /// {@macro figma_variables_api}
+  const FigmaVariablesApi();
+}
---
+++ packages/figma_variables_api/test/src/figma_variables_api_test.dart
@@ -0,0 +1,11 @@
+// ignore_for_file: prefer_const_constructors
+import 'package:test/test.dart';
+import 'package:figma_variables_api/figma_variables_api.dart';
+
+void main() {
+  group('FigmaVariablesApi', () {
+    test('can be instantiated', () {
+      expect(FigmaVariablesApi(), isNotNull);
+    });
+  });
+}

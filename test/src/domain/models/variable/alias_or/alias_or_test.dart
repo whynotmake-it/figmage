e3d66bacaa8469e9ef5f2c7e01e407c77ba3ade6
Tim Lehmenn <hi@timcreated.it>
2023-11-11 16:40:21 +0100

    epic fig

--- test/src/domain/models/variable/alias_or/alias_or_test.dart
+++ test/src/domain/models/variable/alias_or/alias_or_test.dart
@@ -4,22 +4,21 @@ import 'package:test/test.dart';
 void main() {
   group('resolveValue', () {
     test('returns value for data case', () async {
-      const aliasOr = AliasOr.data(data: 42);
+      const aliasOr = AliasData(data: 42);
       expect(aliasOr.resolveValue, equals(42));
     });
 
     test('returns value for single nested alias case', () async {
-      const aliasOr =
-          AliasOr.alias(id: 'someId', aliasOrValue: AliasOr.data(data: 42));
+      const aliasOr = Alias(id: 'someId', aliasOrValue: AliasData(data: 42));
       expect(aliasOr.resolveValue, equals(42));
     });
 
     test('returns value for double nested alias case', () async {
-      const aliasOr = AliasOr.alias(
+      const aliasOr = Alias(
         id: 'someId',
-        aliasOrValue: AliasOr.alias(
+        aliasOrValue: Alias(
           id: 'someId',
-          aliasOrValue: AliasOr.data(data: 42),
+          aliasOrValue: AliasData(data: 42),
         ),
       );
       expect(aliasOr.resolveValue, equals(42));

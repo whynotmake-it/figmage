5325fc33c317c119b3b8418be2b59e47c51895ad
JesperBllnbm <31738080+JesperBllnbm@users.noreply.github.com>
2023-11-11 15:36:07 +0100

    added tests

--- test/src/repositories/figma_variables_repository_test.dart
+++ test/src/repositories/figma_variables_repository_test.dart
@@ -1,217 +1,180 @@
-import 'dart:convert';
-import 'dart:io';
-
 import 'package:figma_variables_api/figma_variables_api.dart';
 import 'package:figmage/src/data/repositories/figma_variables_repository.dart';
+import 'package:figmage/src/domain/models/variable/alias_or/alias_or.dart';
 import 'package:figmage/src/domain/models/variable/variable.dart';
 
 import 'package:test/test.dart';
 
 void main() {
-  //helper function to load json
-  (T, Map<String, dynamic>) parseJsonFromFile<T>(
-    String relativePath,
-    T Function(Map<String, dynamic>) fromJson,
-  ) {
-    final currentDirectory = Directory.current.path;
-    final filePath = '$currentDirectory/test/src/mock_data/$relativePath';
-
-    final jsonString = File(filePath).readAsStringSync();
-    final jsonMap = json.decode(jsonString) as Map<String, dynamic>;
-
-    return (fromJson(jsonMap), jsonMap);
-  }
+  final variablesResponseDto = VariablesResponseDto(
+    status: 200,
+    error: false,
+    meta: VariablesMetaDto(
+      variableCollections: {
+        'VariableCollectionId:0:3': VariableCollectionDto(
+          defaultModeId: '0:0',
+          id: 'VariableCollectionId:0:3',
+          name: 'collection',
+          remote: false,
+          modes: [
+            VariableModeDto(modeId: '0:0', name: 'dark'),
+            VariableModeDto(modeId: '1:0', name: 'light'),
+          ],
+          key: 'c32721b87efc240c6286c911cd457cf0384f7a34',
+          hiddenFromPublishing: false,
+          variableIds: const [
+            'VariableID:1:2',
+            'VariableID:2:3',
+            'VariableID:2:4',
+            'VariableID:2:5',
+          ],
+        ),
+      },
+      variables: {
+        'VariableID:1:2': VariableDto(
+          id: 'VariableID:1:2',
+          name: 'green',
+          remote: false,
+          key: '26a211e627a2da8a80c0f06e0b776ba97a8780e3',
+          variableCollectionId: 'VariableCollectionId:0:3',
+          resolvedType: 'COLOR',
+          description: '',
+          hiddenFromPublishing: false,
+          valuesByMode: {
+            '0:0': VariableModeColorDto(r: 0.708, g: 1, b: 0.0875, a: 1),
+            '1:0': VariableModeColorDto(r: 0.4666, g: 0.7541, b: 0.1006, a: 1),
+          },
+          scopes: const ['ALL_SCOPES'],
+          codeSyntax: const {},
+        ),
+        'VariableID:2:3': VariableDto(
+          id: 'VariableID:2:3',
+          name: 'number',
+          remote: false,
+          key: '08c82898f886945e11a01e68840677696fa360b6',
+          variableCollectionId: 'VariableCollectionId:0:3',
+          resolvedType: 'FLOAT',
+          description: '',
+          hiddenFromPublishing: false,
+          valuesByMode: {
+            '0:0': VariableModeNumberDto(value: 1),
+            '1:0': VariableModeNumberDto(value: 0),
+          },
+          scopes: const ['ALL_SCOPES'],
+          codeSyntax: const {},
+        ),
+        'VariableID:2:4': VariableDto(
+          id: 'VariableID:2:4',
+          name: 'string',
+          remote: false,
+          key: '347bb41e0005e314c7ed7474d85bb804686bed58',
+          variableCollectionId: 'VariableCollectionId:0:3',
+          resolvedType: 'STRING',
+          description: '',
+          hiddenFromPublishing: false,
+          valuesByMode: {
+            '0:0': VariableModeStringDto(value: 'dark'),
+            '1:0': VariableModeStringDto(value: 'light'),
+          },
+          scopes: const ['ALL_SCOPES'],
+          codeSyntax: const {},
+        ),
+        'VariableID:2:5': VariableDto(
+          id: 'VariableID:2:5',
+          name: 'boolean',
+          remote: false,
+          key: '66393690efc9df6a02ed1e813f32e31091597487',
+          variableCollectionId: 'VariableCollectionId:0:3',
+          resolvedType: 'BOOLEAN',
+          description: '',
+          hiddenFromPublishing: false,
+          valuesByMode: {
+            '0:0': VariableModeBooleanDto(value: true),
+            '1:0': VariableModeBooleanDto(value: false),
+          },
+          scopes: const ['ALL_SCOPES'],
+          codeSyntax: const {},
+        ),
+      },
+    ),
+  );
 
   group('fromDtoToModel', () {
-    test('Converts a  VariablesResponse DTO to VariablesData', () {
-      final (variablesResponseDto, _) = parseJsonFromFile(
-        'variables_response_example.json',
-        VariablesResponseDto.fromJson,
-      );
+    test('Converts a  VariablesResponseDto to List<Variable>', () {
       final repository = FigmaVariablesRepository();
       final variables = repository.fromDtoToModel(variablesResponseDto);
 
       expect(variables, isA<List<Variable>>());
+// Specific expected equal statements for each field
+      expect(
+        variables.first.key,
+        equals('26a211e627a2da8a80c0f06e0b776ba97a8780e3'),
+      );
+      expect(variables.first.name, equals('green'));
+      expect(variables.first.remote, equals(false));
+      expect(
+        variables.first.variableCollectionId,
+        equals('VariableCollectionId:0:3'),
+      );
+      expect(variables.first.scopes, equals(const ['ALL_SCOPES']));
+      expect(variables.first.codeSyntax, equals(const {}));
+      expect(variables.first.resolvedType, equals('COLOR'));
+      expect(variables.first.description, equals(''));
+      expect(variables.first.hiddenFromPublishing, equals(false));
+      expect(
+        variables.first.valuesByMode['0:0'],
+        equals(const AliasOr<int>.data(data: 4290117398)),
+      );
+      expect(
+        variables.first.valuesByMode['1:0'],
+        equals(const AliasOr<int>.data(data: 4286038042)),
+      );
     });
+  });
+
+  group('createValueModeMap', () {
+    final repository = FigmaVariablesRepository();
+    final variables = repository.fromDtoToModel(variablesResponseDto);
 
     test('Converts a List<Variable> to a colorMap using names', () {
-      final (variablesResponse, _) = parseJsonFromFile(
-        'variables_response_example.json',
-        VariablesResponseDto.fromJson,
-      );
-      final repository = FigmaVariablesRepository();
-      final variables = repository.fromDtoToModel(variablesResponse);
       final colorMap = repository.createValueModeMap<int, ColorVariable>(
         variables: variables,
       );
-      expect(
-        colorMap['semantic']!['lightMode']!['colorWith2Aliases'],
-        equals(4294967295),
-      );
-      expect(
-        colorMap['semantic']!['lightMode']!['colorWithAlias'],
-        equals(4294967295),
-      );
-      expect(
-        colorMap['semantic']!['lightMode']!['color'],
-        equals(4294967295),
-      );
 
-      expect(
-        colorMap['semantic']!['darkMode']!['colorWith2Aliases'],
-        equals(4278190080),
-      );
-      expect(
-        colorMap['semantic']!['darkMode']!['colorWithAlias'],
-        equals(4278190080),
-      );
-      expect(
-        colorMap['semantic']!['darkMode']!['color'],
-        equals(4278190080),
-      );
+      final expectedColorMap = {
+        'collection': {
+          'dark': {'green': 4290117398},
+          'light': {'green': 4286038042},
+        },
+      };
 
-      expect(
-        colorMap['core']!['Mode 1']!['blue500'],
-        equals(4294967295),
-      );
-      expect(
-        colorMap['core']!['Mode 1']!['blue800'],
-        equals(4278190080),
-      );
-      expect(
-        colorMap['core']!['Mode 1']!['red500'],
-        equals(4294967295),
-      );
-      expect(
-        colorMap['core']!['Mode 1']!['red800'],
-        equals(4278190080),
-      );
-    });
-
-    test('Converts a List<Variable> to a colorMap with ids', () {
-      final (variablesResponse, _) = parseJsonFromFile(
-        'variables_response_example.json',
-        VariablesResponseDto.fromJson,
-      );
-      final repository = FigmaVariablesRepository();
-      final variables = repository.fromDtoToModel(variablesResponse);
-      final colorMap = repository.createValueModeMap<int, ColorVariable>(
-        variables: variables,
-        useNames: false,
-      );
-
-      expect(
-        colorMap['VariableCollectionId:28:10']!['28:3']!['VariableID:28:11'],
-        equals(4294967295),
-      );
-      expect(
-        colorMap['VariableCollectionId:28:10']!['28:3']!['VariableID:33:4'],
-        equals(4294967295),
-      );
-      expect(
-        colorMap['VariableCollectionId:28:10']!['28:3']!['VariableID:114:5'],
-        equals(4294967295),
-      );
-
-      expect(
-        colorMap['VariableCollectionId:28:10']!['28:4']!['VariableID:28:11'],
-        equals(4278190080),
-      );
-      expect(
-        colorMap['VariableCollectionId:28:10']!['28:4']!['VariableID:33:4'],
-        equals(4278190080),
-      );
-      expect(
-        colorMap['VariableCollectionId:28:10']!['28:4']!['VariableID:114:5'],
-        equals(4278190080),
-      );
-
-      expect(
-        colorMap['VariableCollectionId:28:7']!['28:2']!['VariableID:28:9'],
-        equals(4294967295),
-      );
-      expect(
-        colorMap['VariableCollectionId:28:7']!['28:2']!['VariableID:28:12'],
-        equals(4278190080),
-      );
-      expect(
-        colorMap['VariableCollectionId:28:7']!['28:2']!['VariableID:33:2'],
-        equals(4294967295),
-      );
-      expect(
-        colorMap['VariableCollectionId:28:7']!['28:2']!['VariableID:33:3'],
-        equals(4278190080),
-      );
+      expect(colorMap, equals(expectedColorMap));
     });
 
     test('Converts a List<Variable> to a numberMap using names', () {
-      final (variablesResponse, _) = parseJsonFromFile(
-        'variables_response_example.json',
-        VariablesResponseDto.fromJson,
-      );
-      final repository = FigmaVariablesRepository();
-      final variables = repository.fromDtoToModel(variablesResponse);
       final numberMap = repository.createValueModeMap<double, FloatVariable>(
         variables: variables,
       );
-      expect(numberMap['Sizes']!['Mode 1']!['small'], equals(17.0));
-      expect(numberMap['Sizes']!['Mode 2']!['small'], equals(17.0));
-
-      expect(
-        numberMap['semantic']!['lightMode']!['NumberWith2Aliases'],
-        equals(17.0),
-      );
-      expect(
-        numberMap['semantic']!['lightMode']!['NumberWithAlias'],
-        equals(17.0),
-      );
-      expect(numberMap['semantic']!['lightMode']!['number'], equals(17.0));
-
-      expect(
-        numberMap['semantic']!['darkMode']!['NumberWith2Aliases'],
-        equals(17.0),
-      );
-      expect(
-        numberMap['semantic']!['darkMode']!['NumberWithAlias'],
-        equals(17.0),
-      );
-      expect(numberMap['semantic']!['darkMode']!['number'], equals(12.0));
-
-      expect(numberMap['BaseSize']!['light']!['large'], equals(17.0));
-      expect(numberMap['BaseSize']!['dark']!['large'], equals(27.0));
-    });
-
-    test('Converts a List<Variable> to a booleanMap using names', () {
-      final (variablesResponse, _) = parseJsonFromFile(
-        'variables_response_example.json',
-        VariablesResponseDto.fromJson,
-      );
-      final repository = FigmaVariablesRepository();
-      final variables = repository.fromDtoToModel(variablesResponse);
-      final booleanMap = repository.createValueModeMap<bool, BooleanVariable>(
-        variables: variables,
-      );
-
-      expect(booleanMap['semantic']!['lightMode']!['wahr'], equals(false));
-      expect(booleanMap['semantic']!['darkMode']!['wahr'], equals(true));
-      expect(booleanMap['core']!['Mode 1']!['Boolean'], equals(true));
+      final expectedNumberMap = {
+        'collection': {
+          'dark': {'number': 1.0},
+          'light': {'number': 0.0},
+        },
+      };
+      expect(numberMap, equals(expectedNumberMap));
     });
 
     test('Converts a List<Variable> to a stringMap using names', () {
-      final (variablesResponse, _) = parseJsonFromFile(
-        'variables_response_example.json',
-        VariablesResponseDto.fromJson,
-      );
-      final repository = FigmaVariablesRepository();
-      final variables = repository.fromDtoToModel(variablesResponse);
       final stringMap = repository.createValueModeMap<String, StringVariable>(
         variables: variables,
       );
-
-      expect(stringMap['semantic']!['lightMode']!['schrift'], equals('light'));
-      expect(stringMap['semantic']!['darkMode']!['schrift'], equals('dark'));
-      expect(stringMap['core']!['Mode 1']!['light'], equals('light'));
-      expect(stringMap['core']!['Mode 1']!['dark'], equals('dark'));
+      final expectedStringMap = {
+        'collection': {
+          'dark': {'string': 'dark'},
+          'light': {'string': 'light'},
+        },
+      };
+      expect(stringMap, equals(expectedStringMap));
     });
   });
 }

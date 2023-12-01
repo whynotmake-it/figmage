1bda155ab6063c30e22a9b70054bcd85570a1216
Tim Lehmenn <hi@timcreated.it>
2023-12-01 15:12:52 +0100

    FULL COVERAGE

---
+++ packages/figma_variables_api/test/full_coverage_test.dart
@@ -0,0 +1,13 @@
+// ignore_for_file: unused_import
+import 'package:figma_variables_api/figma_variables_api.dart';
+import 'package:figma_variables_api/src/client.dart';
+import 'package:figma_variables_api/src/models.dart';
+import 'package:figma_variables_api/src/models/dto/variable/variable_dto.dart';
+import 'package:figma_variables_api/src/models/dto/variable_collection/variable_collection_dto.dart';
+import 'package:figma_variables_api/src/models/dto/variable_mode/variable_mode_dto.dart';
+import 'package:figma_variables_api/src/models/dto/variable_mode_value/variable_mode_value_dto.dart';
+import 'package:figma_variables_api/src/models/dto/variable_response/variables_response_dto.dart';
+import 'package:figma_variables_api/src/models/dto/variables_meta/variables_meta_dto.dart';
+import 'package:figma_variables_api/src/query.dart';
+
+void main() {}
---
+++ packages/figmage_package_generator/test/full_coverage_test.dart
@@ -0,0 +1,8 @@
+// ignore_for_file: unused_import
+import 'package:figmage_package_generator/figmage_package_generator.dart';
+import 'package:figmage_package_generator/src/brick/__brick__/{{project_name.snakeCase()}}/lib/{{project_name.snakeCase()}}.dart';
+import 'package:figmage_package_generator/src/brick/hooks/post_gen.dart';
+import 'package:figmage_package_generator/src/figmage_package_generator.dart';
+import 'package:figmage_package_generator/src/token_file_type.dart';
+
+void main() {}

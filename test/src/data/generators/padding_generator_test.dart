843f9fda529b23b06aa5c8c819434cc956aa6b85
Tim Lehmann <hi@timcreated.it>
2024-04-15 16:03:43 +0200

    test: fixed padding test

--- test/src/data/generators/padding_generator_test.dart
+++ test/src/data/generators/padding_generator_test.dart
@@ -67,79 +67,79 @@ class WebPadding {
 
   final MyNumbers myNumbers;
 
-  EdgeInsets get sLeft => EdgeInsets.only(left: myNumbers.s);
+  EdgeInsets get sLeft => EdgeInsets.only(left: myNumbers.s!);
 
-  EdgeInsets get sTop => EdgeInsets.only(top: myNumbers.s);
+  EdgeInsets get sTop => EdgeInsets.only(top: myNumbers.s!);
 
-  EdgeInsets get sRight => EdgeInsets.only(right: myNumbers.s);
+  EdgeInsets get sRight => EdgeInsets.only(right: myNumbers.s!);
 
-  EdgeInsets get sBottom => EdgeInsets.only(bottom: myNumbers.s);
+  EdgeInsets get sBottom => EdgeInsets.only(bottom: myNumbers.s!);
 
   EdgeInsets get sVertical => EdgeInsets.only(
-        top: myNumbers.s,
-        bottom: myNumbers.s,
+        top: myNumbers.s!,
+        bottom: myNumbers.s!,
       );
 
   EdgeInsets get sHorizontal => EdgeInsets.only(
-        left: myNumbers.s,
-        right: myNumbers.s,
+        left: myNumbers.s!,
+        right: myNumbers.s!,
       );
 
   EdgeInsets get sAll => EdgeInsets.only(
-        left: myNumbers.s,
-        top: myNumbers.s,
-        right: myNumbers.s,
-        bottom: myNumbers.s,
+        left: myNumbers.s!,
+        top: myNumbers.s!,
+        right: myNumbers.s!,
+        bottom: myNumbers.s!,
       );
 
-  EdgeInsets get mLeft => EdgeInsets.only(left: myNumbers.m);
+  EdgeInsets get mLeft => EdgeInsets.only(left: myNumbers.m!);
 
-  EdgeInsets get mTop => EdgeInsets.only(top: myNumbers.m);
+  EdgeInsets get mTop => EdgeInsets.only(top: myNumbers.m!);
 
-  EdgeInsets get mRight => EdgeInsets.only(right: myNumbers.m);
+  EdgeInsets get mRight => EdgeInsets.only(right: myNumbers.m!);
 
-  EdgeInsets get mBottom => EdgeInsets.only(bottom: myNumbers.m);
+  EdgeInsets get mBottom => EdgeInsets.only(bottom: myNumbers.m!);
 
   EdgeInsets get mVertical => EdgeInsets.only(
-        top: myNumbers.m,
-        bottom: myNumbers.m,
+        top: myNumbers.m!,
+        bottom: myNumbers.m!,
       );
 
   EdgeInsets get mHorizontal => EdgeInsets.only(
-        left: myNumbers.m,
-        right: myNumbers.m,
+        left: myNumbers.m!,
+        right: myNumbers.m!,
       );
 
   EdgeInsets get mAll => EdgeInsets.only(
-        left: myNumbers.m,
-        top: myNumbers.m,
-        right: myNumbers.m,
-        bottom: myNumbers.m,
+        left: myNumbers.m!,
+        top: myNumbers.m!,
+        right: myNumbers.m!,
+        bottom: myNumbers.m!,
       );
 
-  EdgeInsets get lLeft => EdgeInsets.only(left: myNumbers.l);
+  EdgeInsets get lLeft => EdgeInsets.only(left: myNumbers.l!);
 
-  EdgeInsets get lTop => EdgeInsets.only(top: myNumbers.l);
+  EdgeInsets get lTop => EdgeInsets.only(top: myNumbers.l!);
 
-  EdgeInsets get lRight => EdgeInsets.only(right: myNumbers.l);
+  EdgeInsets get lRight => EdgeInsets.only(right: myNumbers.l!);
 
-  EdgeInsets get lBottom => EdgeInsets.only(bottom: myNumbers.l);
+  EdgeInsets get lBottom => EdgeInsets.only(bottom: myNumbers.l!);
 
   EdgeInsets get lVertical => EdgeInsets.only(
-        top: myNumbers.l,
-        bottom: myNumbers.l,
+        top: myNumbers.l!,
+        bottom: myNumbers.l!,
       );
 
   EdgeInsets get lHorizontal => EdgeInsets.only(
-        left: myNumbers.l,
-        right: myNumbers.l,
+        left: myNumbers.l!,
+        right: myNumbers.l!,
       );
 
   EdgeInsets get lAll => EdgeInsets.only(
-        left: myNumbers.l,
-        top: myNumbers.l,
-        right: myNumbers.l,
-        bottom: myNumbers.l,
+        left: myNumbers.l!,
+        top: myNumbers.l!,
+        right: myNumbers.l!,
+        bottom: myNumbers.l!,
       );
 }
 ''';
@@ -155,79 +155,79 @@ class WebPadding {
 
   final MyNumbers myNumbers;
 
-  EdgeInsets get sLeft => EdgeInsets.only(left: myNumbers.s);
+  EdgeInsets get sLeft => EdgeInsets.only(left: myNumbers.s!);
 
-  EdgeInsets get sTop => EdgeInsets.only(top: myNumbers.s);
+  EdgeInsets get sTop => EdgeInsets.only(top: myNumbers.s!);
 
-  EdgeInsets get sRight => EdgeInsets.only(right: myNumbers.s);
+  EdgeInsets get sRight => EdgeInsets.only(right: myNumbers.s!);
 
-  EdgeInsets get sBottom => EdgeInsets.only(bottom: myNumbers.s);
+  EdgeInsets get sBottom => EdgeInsets.only(bottom: myNumbers.s!);
 
   EdgeInsets get sVertical => EdgeInsets.only(
-        top: myNumbers.s,
-        bottom: myNumbers.s,
+        top: myNumbers.s!,
+        bottom: myNumbers.s!,
       );
 
   EdgeInsets get sHorizontal => EdgeInsets.only(
-        left: myNumbers.s,
-        right: myNumbers.s,
+        left: myNumbers.s!,
+        right: myNumbers.s!,
       );
 
   EdgeInsets get sAll => EdgeInsets.only(
-        left: myNumbers.s,
-        top: myNumbers.s,
-        right: myNumbers.s,
-        bottom: myNumbers.s,
+        left: myNumbers.s!,
+        top: myNumbers.s!,
+        right: myNumbers.s!,
+        bottom: myNumbers.s!,
       );
 
-  EdgeInsets get mLeft => EdgeInsets.only(left: myNumbers.m);
+  EdgeInsets get mLeft => EdgeInsets.only(left: myNumbers.m!);
 
-  EdgeInsets get mTop => EdgeInsets.only(top: myNumbers.m);
+  EdgeInsets get mTop => EdgeInsets.only(top: myNumbers.m!);
 
-  EdgeInsets get mRight => EdgeInsets.only(right: myNumbers.m);
+  EdgeInsets get mRight => EdgeInsets.only(right: myNumbers.m!);
 
-  EdgeInsets get mBottom => EdgeInsets.only(bottom: myNumbers.m);
+  EdgeInsets get mBottom => EdgeInsets.only(bottom: myNumbers.m!);
 
   EdgeInsets get mVertical => EdgeInsets.only(
-        top: myNumbers.m,
-        bottom: myNumbers.m,
+        top: myNumbers.m!,
+        bottom: myNumbers.m!,
       );
 
   EdgeInsets get mHorizontal => EdgeInsets.only(
-        left: myNumbers.m,
-        right: myNumbers.m,
+        left: myNumbers.m!,
+        right: myNumbers.m!,
       );
 
   EdgeInsets get mAll => EdgeInsets.only(
-        left: myNumbers.m,
-        top: myNumbers.m,
-        right: myNumbers.m,
-        bottom: myNumbers.m,
+        left: myNumbers.m!,
+        top: myNumbers.m!,
+        right: myNumbers.m!,
+        bottom: myNumbers.m!,
       );
 
-  EdgeInsets get lLeft => EdgeInsets.only(left: myNumbers.l);
+  EdgeInsets get lLeft => EdgeInsets.only(left: myNumbers.l!);
 
-  EdgeInsets get lTop => EdgeInsets.only(top: myNumbers.l);
+  EdgeInsets get lTop => EdgeInsets.only(top: myNumbers.l!);
 
-  EdgeInsets get lRight => EdgeInsets.only(right: myNumbers.l);
+  EdgeInsets get lRight => EdgeInsets.only(right: myNumbers.l!);
 
-  EdgeInsets get lBottom => EdgeInsets.only(bottom: myNumbers.l);
+  EdgeInsets get lBottom => EdgeInsets.only(bottom: myNumbers.l!);
 
   EdgeInsets get lVertical => EdgeInsets.only(
-        top: myNumbers.l,
-        bottom: myNumbers.l,
+        top: myNumbers.l!,
+        bottom: myNumbers.l!,
       );
 
   EdgeInsets get lHorizontal => EdgeInsets.only(
-        left: myNumbers.l,
-        right: myNumbers.l,
+        left: myNumbers.l!,
+        right: myNumbers.l!,
       );
 
   EdgeInsets get lAll => EdgeInsets.only(
-        left: myNumbers.l,
-        top: myNumbers.l,
-        right: myNumbers.l,
-        bottom: myNumbers.l,
+        left: myNumbers.l!,
+        top: myNumbers.l!,
+        right: myNumbers.l!,
+        bottom: myNumbers.l!,
       );
 }
 ''';

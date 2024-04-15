d88cff84471505454bb578e913f23f1f54cf51ba
Tim Lehmann <hi@timcreated.it>
2024-04-15 15:00:55 +0200

    fix: updated generated files

--- lib/src/domain/models/variable/alias_or/alias_or.freezed.dart
+++ lib/src/domain/models/variable/alias_or/alias_or.freezed.dart
@@ -12,7 +12,7 @@ part of 'alias_or.dart';
 T _$identity<T>(T value) => value;
 
 final _privateConstructorUsedError = UnsupportedError(
-    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more information: https://github.com/rrousselGit/freezed#custom-getters-and-methods');
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more information: https://github.com/rrousselGit/freezed#adding-getters-and-methods-to-our-models');
 
 /// @nodoc
 mixin _$AliasOr<T> {
@@ -118,7 +118,7 @@ class _$AliasDataImpl<T> extends AliasData<T> {
   }
 
   @override
-  bool operator ==(dynamic other) {
+  bool operator ==(Object other) {
     return identical(this, other) ||
         (other.runtimeType == runtimeType &&
             other is _$AliasDataImpl<T> &&
@@ -270,7 +270,7 @@ class _$AliasImpl<T> extends Alias<T> {
   }
 
   @override
-  bool operator ==(dynamic other) {
+  bool operator ==(Object other) {
     return identical(this, other) ||
         (other.runtimeType == runtimeType &&
             other is _$AliasImpl<T> &&

e61e350ecfe7dab6a02e084d5b405e28de3152f8
Tim Lehmann <hi@timcreated.it>
2024-05-04 12:33:09 +0200

    added more tests to node_content

--- packages/figma_variables_api/test/src/dto/nodes/node_content_test.dart
+++ packages/figma_variables_api/test/src/dto/nodes/node_content_test.dart
@@ -48,5 +48,100 @@ void main() {
         expect(result.document, isA<Slice>());
       });
     });
+
+    group('toJson', () {
+      test('should return a JSON map containing proper data', () {
+        final nodeContent = NodeContent(
+          name: 'name',
+          role: 'role',
+          lastModified: DateTime.parse('2021-08-06T00:00:00Z'),
+          thumbnailUrl: 'thumbnailUrl',
+          version: 'version',
+          document: null,
+          components: <String, Component>{},
+          componentSets: <String, ComponentSet>{},
+          schemaVersion: 1,
+          styles: <String, Style>{},
+        );
+
+        final result = nodeContent.toJson();
+        expect(result, isA<Map<String, dynamic>>());
+        expect(result["name"], nodeContent.name);
+        expect(result["role"], nodeContent.role);
+        expect(
+          result["lastModified"],
+          nodeContent.lastModified?.toIso8601String(),
+        );
+        expect(result["thumbnailUrl"], nodeContent.thumbnailUrl);
+        expect(result["version"], nodeContent.version);
+        expect(result["document"], nodeContent.document);
+        expect(result["components"], nodeContent.components);
+        expect(result["componentSets"], nodeContent.componentSets);
+        expect(result["schemaVersion"], nodeContent.schemaVersion);
+        expect(result["styles"], nodeContent.styles);
+      });
+    });
+
+    group('equality', () {
+      test('should be equal if all properties are equal', () {
+        final nodeContent1 = NodeContent(
+          name: 'name',
+          role: 'role',
+          lastModified: DateTime.parse('2021-08-06T00:00:00Z'),
+          thumbnailUrl: 'thumbnailUrl',
+          version: 'version',
+          document: null,
+          components: <String, Component>{},
+          componentSets: <String, ComponentSet>{},
+          schemaVersion: 1,
+          styles: <String, Style>{},
+        );
+
+        final nodeContent2 = NodeContent(
+          name: 'name',
+          role: 'role',
+          lastModified: DateTime.parse('2021-08-06T00:00:00Z'),
+          thumbnailUrl: 'thumbnailUrl',
+          version: 'version',
+          document: null,
+          components: <String, Component>{},
+          componentSets: <String, ComponentSet>{},
+          schemaVersion: 1,
+          styles: <String, Style>{},
+        );
+
+        expect(nodeContent1, nodeContent2);
+      });
+
+      test('should not be equal if any property is different', () {
+        final nodeContent1 = NodeContent(
+          name: 'name',
+          role: 'role',
+          lastModified: DateTime.parse('2021-08-06T00:00:00Z'),
+          thumbnailUrl: 'thumbnailUrl',
+          version: 'version',
+          document: null,
+          components: <String, Component>{},
+          componentSets: <String, ComponentSet>{},
+          schemaVersion: 1,
+          styles: <String, Style>{},
+        );
+
+        final nodeContent2 = NodeContent(
+          name: 'name',
+          role: 'role',
+          lastModified: DateTime.parse('2021-08-06T00:00:00Z'),
+          thumbnailUrl: 'thumbnailUrl',
+          version: 'version',
+          document: null,
+          components: <String, Component>{},
+          componentSets: <String, ComponentSet>{},
+          schemaVersion: 1,
+          styles: <String, Style>{},
+        );
+
+        expect(nodeContent1, nodeContent2);
+      });
+    });
   });
 }

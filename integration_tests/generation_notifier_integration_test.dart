4d7a8516707038cd0bbf57e3ebd2cbf41e06430b
Tim Lehmann <hi@timcreated.it>
2024-09-18 22:31:23 +0200

    test: integration tests for generation can run now

--- integration_tests/generation_notifier_integration_test.dart
+++ integration_tests/generation_notifier_integration_test.dart
@@ -1,8 +1,15 @@
 import 'dart:io';
 
 import 'package:figmage/src/command_runner.dart';
+import 'package:figmage/src/domain/models/config/config.dart';
+import 'package:figmage/src/domain/providers/config_providers.dart';
 import 'package:riverpod/riverpod.dart';
 import 'package:test/test.dart';
+
+import '../test/test_util/create_container.dart';
+
+/// A file that contains only local styles, no library styles.
+const localStylesFileId = "HHUVTJ7lsjhG24SQB5h0zX";
 
 void main() {
   group("GenerationNotifier", () {
@@ -11,45 +18,67 @@ void main() {
     late String? fileId;
 
     late Directory testDirectory;
+
+    late Config config;
+
     setUp(() {
-      testDirectory = Directory("${Directory.current.path}/test_generated");
-      container = ProviderContainer();
+      config = const Config();
+      testDirectory = Directory("${Directory.current.path}/figmage_package");
+      container = createContainer(
+        overrides: [
+          configProvider.overrideWith((ref, arg) => config),
+        ],
+      );
       token = Platform.environment['FIGMA_TOKEN'];
-      fileId = Platform.environment['FIGMA_FILE_ID'];
     });
 
     tearDown(() {
-      container.dispose();
       try {
         testDirectory.deleteSync(recursive: true);
       } catch (_) {}
     });
 
-    group('can generate variables from test file', () {
+    Future<void> run() async {
+      final runner = FigmageCommandRunner(container);
+      await runner.run([
+        'forge',
+        '--token',
+        token!,
+        '--fileId',
+        fileId!,
+        '--path',
+        testDirectory.path,
+      ]);
+    }
+
+    group('can generate from local styles file', () {
+      setUp(() {
+        fileId = localStylesFileId;
+      });
+
       test(
-        'should generate variables',
+        'should generate unpublished styles by default',
         timeout: const Timeout(Duration(minutes: 1)),
         () async {
-          final runner = FigmageCommandRunner(container);
-          await runner.run([
-            'forge',
-            '--token',
-            token!,
-            '--fileId',
-            fileId!,
-            '--path',
-            testDirectory.path,
-          ]);
-
+          await run();
           final files = [
-            File("${testDirectory.path}/figmage_package/lib/src/colors.dart"),
+            File("${testDirectory.path}/lib/src/colors.dart"),
             File(
-              "${testDirectory.path}/figmage_package/lib/src/typography.dart",
+              "${testDirectory.path}/lib/src/typography.dart",
             ),
           ];
           expect(files.every((f) => f.existsSync()), true);
         },
       );
     });
+
+    test(
+      'generates no styles if stylesFromLibrary is true',
+      timeout: const Timeout(Duration(minutes: 1)),
+      () async {
+        config = const Config(stylesFromLibrary: true);
+        await expectLater(run, throwsArgumentError);
+      },
+    );
   });
 }

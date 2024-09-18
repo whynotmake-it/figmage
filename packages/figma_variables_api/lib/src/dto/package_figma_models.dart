505b98912f4887afe3db31eeab88f881766269d1
Tim Lehmann <hi@timcreated.it>
2024-09-18 14:27:04 +0200

    feat(figma_variables_api): support file endpoint

--- packages/figma_variables_api/lib/src/dto/package_figma_models.dart
+++ packages/figma_variables_api/lib/src/dto/package_figma_models.dart
@@ -1,11 +1,6 @@
-/// Models that are **illegaly** exported from the `figma` package.
+/// Models that are exported from the `figma` package.
 ///
 /// Until the `figma` package is fixed to work for Node queries, this is all we
 /// have.
-export 'package:figma/src/models/node.dart';
-export 'package:figma/src/models/text.dart';
-export 'package:figma/src/models/rectangle.dart';
-export 'package:figma/src/models/paint.dart';
-export 'package:figma/src/models/color.dart';
-export 'package:figma/src/models/style.dart';
-export 'package:figma/src/models/type_style.dart';
+export 'package:figma/figma.dart'
+    show FileResponse, Node, Text, Rectangle, Paint, Color, Style, TypeStyle;

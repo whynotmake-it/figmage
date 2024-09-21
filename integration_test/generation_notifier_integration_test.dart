3974277fc3d799a586bee9dc6160bd42e3fe4025
Tim Lehmann <hi@timcreated.it>
2024-09-21 14:41:57 +0200

    test: updated integration test to verify the amount of tokens
    The test file also contains a style from an external library now.

--- integration_test/generation_notifier_integration_test.dart
+++ integration_test/generation_notifier_integration_test.dart
@@ -1,8 +1,10 @@
 import 'dart:io';
 
 import 'package:figmage/src/command_runner.dart';
+import 'package:figmage/src/commands/shared/forge_settings_providers.dart';
 import 'package:figmage/src/domain/models/config/config.dart';
 import 'package:figmage/src/domain/providers/config_providers.dart';
+import 'package:figmage/src/domain/providers/design_token_providers.dart';
 import 'package:riverpod/riverpod.dart';
 import 'package:test/test.dart';
 
@@ -15,11 +17,14 @@ void main() {
   group("GenerationNotifier", () {
     late ProviderContainer container;
     late String? token;
-    late String? fileId;
 
     late Directory testDirectory;
 
     late Config config;
+
+    late List<String> args;
+
+    late FigmageCommandRunner runner;
 
     setUp(() {
       config = const Config();
@@ -30,6 +35,7 @@ void main() {
         ],
       );
       token = Platform.environment['FIGMA_FREE_TOKEN'];
+      runner = FigmageCommandRunner(container);
     });
 
     tearDown(() {
@@ -38,35 +44,36 @@ void main() {
       } catch (_) {}
     });
 
-    Future<void> run() async {
-      final runner = FigmageCommandRunner(container);
-      await runner.run([
-        'forge',
-        '--token',
-        token!,
-        '--fileId',
-        fileId!,
-        '--path',
-        testDirectory.path,
-      ]);
-    }
-
     group('can generate from local styles file', () {
       setUp(() {
-        fileId = localStylesFileId;
+        args = [
+          'forge',
+          '--token',
+          token!,
+          '--fileId',
+          localStylesFileId,
+          '--path',
+          testDirectory.path,
+        ];
       });
 
       test(
-        'should generate unpublished styles by default',
+        'should generate 7 unpublished styles by default',
         timeout: const Timeout(Duration(minutes: 1)),
         () async {
-          await run();
+          await runner.run(args);
           final files = [
             File("${testDirectory.path}/lib/src/colors.dart"),
             File(
               "${testDirectory.path}/lib/src/typography.dart",
             ),
           ];
+          final argResults = runner.parse(args).command!;
+          final settings =
+              await container.read(settingsProvider(argResults).future);
+          final styles = await container.read(stylesProvider(settings).future);
+
+          expect(styles, hasLength(7));
           expect(files.every((f) => f.existsSync()), true);
         },
       );
@@ -77,7 +84,7 @@ void main() {
       timeout: const Timeout(Duration(minutes: 1)),
       () async {
         config = const Config(stylesFromLibrary: true);
-        await expectLater(run, throwsArgumentError);
+        await expectLater(runner.run(args), throwsArgumentError);
       },
     );
   });

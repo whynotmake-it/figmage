3db1e4933652e282d470ac0956b889f4ac2956cd
Tim Lehmann <tim.lehmann@loql.com>
2025-05-22 10:42:38 +0200

    chore: fixed listening to the wrong stream

--- integration_test/full_integration_test.dart
+++ integration_test/full_integration_test.dart
@@ -65,8 +65,8 @@ void main() {
         workingDirectory: testDir.path,
       );
 
-      process.stdout.transform(utf8.decoder).listen(logger.info);
-      process.stderr.transform(utf8.decoder).listen(logger.err);
+      analyzeProcess.stdout.transform(utf8.decoder).listen(logger.info);
+      analyzeProcess.stderr.transform(utf8.decoder).listen(logger.err);
 
       final analyzeExitCode = await analyzeProcess.exitCode;
       expect(

a8642aa981157cb0486b40f4d4c7c6cd88a5d1bb
Tim Lehmann <tim.lehmann@loql.com>
2025-05-22 11:30:42 +0200

    docs: update website theme

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -1,27 +1,32 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
+import starlightThemeRapidePlugin from 'starlight-theme-rapide';
 
 // https://astro.build/config
 export default defineConfig({
 	integrations: [
 		starlight({
+			plugins: [starlightThemeRapidePlugin()],
 			title: 'Figmage',
-			social: {
-				github: 'https://github.com/whynotmake-it/figmage',
-			},
+			social: [
+				{
+					icon: 'github',
+					label: 'GitHub',
+					href: 'https://github.com/whynotmake-it/figmage',
+				}
+			],
 			sidebar: [
 				{
 					label: 'Guides',
 					autogenerate: { directory: 'guides' },
-					
 				},
 				{
 					label: 'Reference',
 					autogenerate: { directory: 'reference' },
 				},
 			],
-			
+
 		}),
 	],
 });

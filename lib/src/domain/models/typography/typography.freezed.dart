86a77311076af5ba9273cf76d7f9954d5e677e88
Tim Lehmann <tim.lehmann@loql.com>
2025-06-06 09:40:17 +0200

    chore: generate

--- lib/src/domain/models/typography/typography.freezed.dart
+++ lib/src/domain/models/typography/typography.freezed.dart
@@ -1,3 +1,4 @@
+// dart format width=80
 // coverage:ignore-file
 // GENERATED CODE - DO NOT MODIFY BY HAND
 // ignore_for_file: type=lint
@@ -9,121 +10,77 @@ part of 'typography.dart';
 // FreezedGenerator
 // **************************************************************************
 
+// dart format off
 T _$identity<T>(T value) => value;
-
-final _privateConstructorUsedError = UnsupportedError(
-    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more information: https://github.com/rrousselGit/freezed#adding-getters-and-methods-to-our-models');
 
 /// @nodoc
 mixin _$Typography {
-  String get fontFamily => throw _privateConstructorUsedError;
-  String? get fontFamilyPostScriptName => throw _privateConstructorUsedError;
-  double get fontSize => throw _privateConstructorUsedError;
-  int get fontWeight => throw _privateConstructorUsedError;
-  TextDecoration get decoration => throw _privateConstructorUsedError;
-  FontStyle get fontStyle => throw _privateConstructorUsedError;
-  double get letterSpacing => throw _privateConstructorUsedError;
-  double get wordSpacing => throw _privateConstructorUsedError;
-  double get height => throw _privateConstructorUsedError;
+  String get fontFamily;
+  String? get fontFamilyPostScriptName;
+  double get fontSize;
+  int get fontWeight;
+  TextDecoration get decoration;
+  FontStyle get fontStyle;
+  double get letterSpacing;
+  double get wordSpacing;
+  double get height;
 
   /// Create a copy of Typography
   /// with the given fields replaced by the non-null parameter values.
   @JsonKey(includeFromJson: false, includeToJson: false)
-  $TypographyCopyWith<Typography> get copyWith =>
-      throw _privateConstructorUsedError;
-}
-
-/// @nodoc
-abstract class $TypographyCopyWith<$Res> {
-  factory $TypographyCopyWith(
-          Typography value, $Res Function(Typography) then) =
-      _$TypographyCopyWithImpl<$Res, Typography>;
-  @useResult
-  $Res call(
-      {String fontFamily,
-      String? fontFamilyPostScriptName,
-      double fontSize,
-      int fontWeight,
-      TextDecoration decoration,
-      FontStyle fontStyle,
-      double letterSpacing,
-      double wordSpacing,
-      double height});
-}
-
-/// @nodoc
-class _$TypographyCopyWithImpl<$Res, $Val extends Typography>
-    implements $TypographyCopyWith<$Res> {
-  _$TypographyCopyWithImpl(this._value, this._then);
-
-  // ignore: unused_field
-  final $Val _value;
-  // ignore: unused_field
-  final $Res Function($Val) _then;
-
-  /// Create a copy of Typography
-  /// with the given fields replaced by the non-null parameter values.
   @pragma('vm:prefer-inline')
+  $TypographyCopyWith<Typography> get copyWith =>
+      _$TypographyCopyWithImpl<Typography>(this as Typography, _$identity);
+
   @override
-  $Res call({
-    Object? fontFamily = null,
-    Object? fontFamilyPostScriptName = freezed,
-    Object? fontSize = null,
-    Object? fontWeight = null,
-    Object? decoration = null,
-    Object? fontStyle = null,
-    Object? letterSpacing = null,
-    Object? wordSpacing = null,
-    Object? height = null,
-  }) {
-    return _then(_value.copyWith(
-      fontFamily: null == fontFamily
-          ? _value.fontFamily
-          : fontFamily // ignore: cast_nullable_to_non_nullable
-              as String,
-      fontFamilyPostScriptName: freezed == fontFamilyPostScriptName
-          ? _value.fontFamilyPostScriptName
-          : fontFamilyPostScriptName // ignore: cast_nullable_to_non_nullable
-              as String?,
-      fontSize: null == fontSize
-          ? _value.fontSize
-          : fontSize // ignore: cast_nullable_to_non_nullable
-              as double,
-      fontWeight: null == fontWeight
-          ? _value.fontWeight
-          : fontWeight // ignore: cast_nullable_to_non_nullable
-              as int,
-      decoration: null == decoration
-          ? _value.decoration
-          : decoration // ignore: cast_nullable_to_non_nullable
-              as TextDecoration,
-      fontStyle: null == fontStyle
-          ? _value.fontStyle
-          : fontStyle // ignore: cast_nullable_to_non_nullable
-              as FontStyle,
-      letterSpacing: null == letterSpacing
-          ? _value.letterSpacing
-          : letterSpacing // ignore: cast_nullable_to_non_nullable
-              as double,
-      wordSpacing: null == wordSpacing
-          ? _value.wordSpacing
-          : wordSpacing // ignore: cast_nullable_to_non_nullable
-              as double,
-      height: null == height
-          ? _value.height
-          : height // ignore: cast_nullable_to_non_nullable
-              as double,
-    ) as $Val);
+  bool operator ==(Object other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType &&
+            other is Typography &&
+            (identical(other.fontFamily, fontFamily) ||
+                other.fontFamily == fontFamily) &&
+            (identical(
+                    other.fontFamilyPostScriptName, fontFamilyPostScriptName) ||
+                other.fontFamilyPostScriptName == fontFamilyPostScriptName) &&
+            (identical(other.fontSize, fontSize) ||
+                other.fontSize == fontSize) &&
+            (identical(other.fontWeight, fontWeight) ||
+                other.fontWeight == fontWeight) &&
+            (identical(other.decoration, decoration) ||
+                other.decoration == decoration) &&
+            (identical(other.fontStyle, fontStyle) ||
+                other.fontStyle == fontStyle) &&
+            (identical(other.letterSpacing, letterSpacing) ||
+                other.letterSpacing == letterSpacing) &&
+            (identical(other.wordSpacing, wordSpacing) ||
+                other.wordSpacing == wordSpacing) &&
+            (identical(other.height, height) || other.height == height));
+  }
+
+  @override
+  int get hashCode => Object.hash(
+      runtimeType,
+      fontFamily,
+      fontFamilyPostScriptName,
+      fontSize,
+      fontWeight,
+      decoration,
+      fontStyle,
+      letterSpacing,
+      wordSpacing,
+      height);
+
+  @override
+  String toString() {
+    return 'Typography(fontFamily: $fontFamily, fontFamilyPostScriptName: $fontFamilyPostScriptName, fontSize: $fontSize, fontWeight: $fontWeight, decoration: $decoration, fontStyle: $fontStyle, letterSpacing: $letterSpacing, wordSpacing: $wordSpacing, height: $height)';
   }
 }
 
 /// @nodoc
-abstract class _$$TypographyImplCopyWith<$Res>
-    implements $TypographyCopyWith<$Res> {
-  factory _$$TypographyImplCopyWith(
-          _$TypographyImpl value, $Res Function(_$TypographyImpl) then) =
-      __$$TypographyImplCopyWithImpl<$Res>;
-  @override
+abstract mixin class $TypographyCopyWith<$Res> {
+  factory $TypographyCopyWith(
+          Typography value, $Res Function(Typography) _then) =
+      _$TypographyCopyWithImpl;
   @useResult
   $Res call(
       {String fontFamily,
@@ -138,12 +95,11 @@ abstract class _$$TypographyImplCopyWith<$Res>
 }
 
 /// @nodoc
-class __$$TypographyImplCopyWithImpl<$Res>
-    extends _$TypographyCopyWithImpl<$Res, _$TypographyImpl>
-    implements _$$TypographyImplCopyWith<$Res> {
-  __$$TypographyImplCopyWithImpl(
-      _$TypographyImpl _value, $Res Function(_$TypographyImpl) _then)
-      : super(_value, _then);
+class _$TypographyCopyWithImpl<$Res> implements $TypographyCopyWith<$Res> {
+  _$TypographyCopyWithImpl(this._self, this._then);
+
+  final Typography _self;
+  final $Res Function(Typography) _then;
 
   /// Create a copy of Typography
   /// with the given fields replaced by the non-null parameter values.
@@ -160,41 +116,41 @@ class __$$TypographyImplCopyWithImpl<$Res>
     Object? wordSpacing = null,
     Object? height = null,
   }) {
-    return _then(_$TypographyImpl(
+    return _then(_self.copyWith(
       fontFamily: null == fontFamily
-          ? _value.fontFamily
+          ? _self.fontFamily
           : fontFamily // ignore: cast_nullable_to_non_nullable
               as String,
       fontFamilyPostScriptName: freezed == fontFamilyPostScriptName
-          ? _value.fontFamilyPostScriptName
+          ? _self.fontFamilyPostScriptName
           : fontFamilyPostScriptName // ignore: cast_nullable_to_non_nullable
               as String?,
       fontSize: null == fontSize
-          ? _value.fontSize
+          ? _self.fontSize
           : fontSize // ignore: cast_nullable_to_non_nullable
               as double,
       fontWeight: null == fontWeight
-          ? _value.fontWeight
+          ? _self.fontWeight
           : fontWeight // ignore: cast_nullable_to_non_nullable
               as int,
       decoration: null == decoration
-          ? _value.decoration
+          ? _self.decoration
           : decoration // ignore: cast_nullable_to_non_nullable
               as TextDecoration,
       fontStyle: null == fontStyle
-          ? _value.fontStyle
+          ? _self.fontStyle
           : fontStyle // ignore: cast_nullable_to_non_nullable
               as FontStyle,
       letterSpacing: null == letterSpacing
-          ? _value.letterSpacing
+          ? _self.letterSpacing
           : letterSpacing // ignore: cast_nullable_to_non_nullable
               as double,
       wordSpacing: null == wordSpacing
-          ? _value.wordSpacing
+          ? _self.wordSpacing
           : wordSpacing // ignore: cast_nullable_to_non_nullable
               as double,
       height: null == height
-          ? _value.height
+          ? _self.height
           : height // ignore: cast_nullable_to_non_nullable
               as double,
     ));
@@ -203,8 +159,8 @@ class __$$TypographyImplCopyWithImpl<$Res>
 
 /// @nodoc
 
-class _$TypographyImpl extends _Typography {
-  const _$TypographyImpl(
+class _Typography extends Typography {
+  const _Typography(
       {required this.fontFamily,
       required this.fontFamilyPostScriptName,
       required this.fontSize,
@@ -241,16 +197,19 @@ class _$TypographyImpl extends _Typography {
   @JsonKey()
   final double height;
 
+  /// Create a copy of Typography
+  /// with the given fields replaced by the non-null parameter values.
   @override
-  String toString() {
-    return 'Typography(fontFamily: $fontFamily, fontFamilyPostScriptName: $fontFamilyPostScriptName, fontSize: $fontSize, fontWeight: $fontWeight, decoration: $decoration, fontStyle: $fontStyle, letterSpacing: $letterSpacing, wordSpacing: $wordSpacing, height: $height)';
-  }
+  @JsonKey(includeFromJson: false, includeToJson: false)
+  @pragma('vm:prefer-inline')
+  _$TypographyCopyWith<_Typography> get copyWith =>
+      __$TypographyCopyWithImpl<_Typography>(this, _$identity);
 
   @override
   bool operator ==(Object other) {
     return identical(this, other) ||
         (other.runtimeType == runtimeType &&
-            other is _$TypographyImpl &&
+            other is _Typography &&
             (identical(other.fontFamily, fontFamily) ||
                 other.fontFamily == fontFamily) &&
             (identical(
@@ -284,51 +243,93 @@ class _$TypographyImpl extends _Typography {
       wordSpacing,
       height);
 
+  @override
+  String toString() {
+    return 'Typography(fontFamily: $fontFamily, fontFamilyPostScriptName: $fontFamilyPostScriptName, fontSize: $fontSize, fontWeight: $fontWeight, decoration: $decoration, fontStyle: $fontStyle, letterSpacing: $letterSpacing, wordSpacing: $wordSpacing, height: $height)';
+  }
+}
+
+/// @nodoc
+abstract mixin class _$TypographyCopyWith<$Res>
+    implements $TypographyCopyWith<$Res> {
+  factory _$TypographyCopyWith(
+          _Typography value, $Res Function(_Typography) _then) =
+      __$TypographyCopyWithImpl;
+  @override
+  @useResult
+  $Res call(
+      {String fontFamily,
+      String? fontFamilyPostScriptName,
+      double fontSize,
+      int fontWeight,
+      TextDecoration decoration,
+      FontStyle fontStyle,
+      double letterSpacing,
+      double wordSpacing,
+      double height});
+}
+
+/// @nodoc
+class __$TypographyCopyWithImpl<$Res> implements _$TypographyCopyWith<$Res> {
+  __$TypographyCopyWithImpl(this._self, this._then);
+
+  final _Typography _self;
+  final $Res Function(_Typography) _then;
+
   /// Create a copy of Typography
   /// with the given fields replaced by the non-null parameter values.
-  @JsonKey(includeFromJson: false, includeToJson: false)
   @override
   @pragma('vm:prefer-inline')
-  _$$TypographyImplCopyWith<_$TypographyImpl> get copyWith =>
-      __$$TypographyImplCopyWithImpl<_$TypographyImpl>(this, _$identity);
+  $Res call({
+    Object? fontFamily = null,
+    Object? fontFamilyPostScriptName = freezed,
+    Object? fontSize = null,
+    Object? fontWeight = null,
+    Object? decoration = null,
+    Object? fontStyle = null,
+    Object? letterSpacing = null,
+    Object? wordSpacing = null,
+    Object? height = null,
+  }) {
+    return _then(_Typography(
+      fontFamily: null == fontFamily
+          ? _self.fontFamily
+          : fontFamily // ignore: cast_nullable_to_non_nullable
+              as String,
+      fontFamilyPostScriptName: freezed == fontFamilyPostScriptName
+          ? _self.fontFamilyPostScriptName
+          : fontFamilyPostScriptName // ignore: cast_nullable_to_non_nullable
+              as String?,
+      fontSize: null == fontSize
+          ? _self.fontSize
+          : fontSize // ignore: cast_nullable_to_non_nullable
+              as double,
+      fontWeight: null == fontWeight
+          ? _self.fontWeight
+          : fontWeight // ignore: cast_nullable_to_non_nullable
+              as int,
+      decoration: null == decoration
+          ? _self.decoration
+          : decoration // ignore: cast_nullable_to_non_nullable
+              as TextDecoration,
+      fontStyle: null == fontStyle
+          ? _self.fontStyle
+          : fontStyle // ignore: cast_nullable_to_non_nullable
+              as FontStyle,
+      letterSpacing: null == letterSpacing
+          ? _self.letterSpacing
+          : letterSpacing // ignore: cast_nullable_to_non_nullable
+              as double,
+      wordSpacing: null == wordSpacing
+          ? _self.wordSpacing
+          : wordSpacing // ignore: cast_nullable_to_non_nullable
+              as double,
+      height: null == height
+          ? _self.height
+          : height // ignore: cast_nullable_to_non_nullable
+              as double,
+    ));
+  }
 }
 
-abstract class _Typography extends Typography {
-  const factory _Typography(
-      {required final String fontFamily,
-      required final String? fontFamilyPostScriptName,
-      required final double fontSize,
-      final int fontWeight,
-      final TextDecoration decoration,
-      final FontStyle fontStyle,
-      final double letterSpacing,
-      final double wordSpacing,
-      final double height}) = _$TypographyImpl;
-  const _Typography._() : super._();
-
-  @override
-  String get fontFamily;
-  @override
-  String? get fontFamilyPostScriptName;
-  @override
-  double get fontSize;
-  @override
-  int get fontWeight;
-  @override
-  TextDecoration get decoration;
-  @override
-  FontStyle get fontStyle;
-  @override
-  double get letterSpacing;
-  @override
-  double get wordSpacing;
-  @override
-  double get height;
-
-  /// Create a copy of Typography
-  /// with the given fields replaced by the non-null parameter values.
-  @override
-  @JsonKey(includeFromJson: false, includeToJson: false)
-  _$$TypographyImplCopyWith<_$TypographyImpl> get copyWith =>
-      throw _privateConstructorUsedError;
-}
+// dart format on

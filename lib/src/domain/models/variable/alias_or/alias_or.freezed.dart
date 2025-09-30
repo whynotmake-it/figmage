b3b65a6d69c598d0a0cd8f967848d3841ccdbd61
Jesper Bellenbaum <jesper@wearetoni.com>
2025-09-30 11:19:20 +0200

    fix: bump freezed version

--- lib/src/domain/models/variable/alias_or/alias_or.freezed.dart
+++ lib/src/domain/models/variable/alias_or/alias_or.freezed.dart
@@ -1,6 +1,5 @@
-// dart format width=80
-// coverage:ignore-file
 // GENERATED CODE - DO NOT MODIFY BY HAND
+// coverage:ignore-file
 // ignore_for_file: type=lint
 // ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target, unnecessary_question_mark
 
@@ -33,6 +32,195 @@ mixin _$AliasOr<T> {
 /// @nodoc
 class $AliasOrCopyWith<T, $Res> {
   $AliasOrCopyWith(AliasOr<T> _, $Res Function(AliasOr<T>) __);
+}
+
+/// Adds pattern-matching-related methods to [AliasOr].
+extension AliasOrPatterns<T> on AliasOr<T> {
+  /// A variant of `map` that fallback to returning `orElse`.
+  ///
+  /// It is equivalent to doing:
+  /// ```dart
+  /// switch (sealedClass) {
+  ///   case final Subclass value:
+  ///     return ...;
+  ///   case _:
+  ///     return orElse();
+  /// }
+  /// ```
+
+  @optionalTypeArgs
+  TResult maybeMap<TResult extends Object?>({
+    TResult Function(AliasData<T> value)? data,
+    TResult Function(Alias<T> value)? alias,
+    TResult Function(AliasUnresolved<T> value)? unresolved,
+    required TResult orElse(),
+  }) {
+    final _that = this;
+    switch (_that) {
+      case AliasData() when data != null:
+        return data(_that);
+      case Alias() when alias != null:
+        return alias(_that);
+      case AliasUnresolved() when unresolved != null:
+        return unresolved(_that);
+      case _:
+        return orElse();
+    }
+  }
+
+  /// A `switch`-like method, using callbacks.
+  ///
+  /// Callbacks receives the raw object, upcasted.
+  /// It is equivalent to doing:
+  /// ```dart
+  /// switch (sealedClass) {
+  ///   case final Subclass value:
+  ///     return ...;
+  ///   case final Subclass2 value:
+  ///     return ...;
+  /// }
+  /// ```
+
+  @optionalTypeArgs
+  TResult map<TResult extends Object?>({
+    required TResult Function(AliasData<T> value) data,
+    required TResult Function(Alias<T> value) alias,
+    required TResult Function(AliasUnresolved<T> value) unresolved,
+  }) {
+    final _that = this;
+    switch (_that) {
+      case AliasData():
+        return data(_that);
+      case Alias():
+        return alias(_that);
+      case AliasUnresolved():
+        return unresolved(_that);
+    }
+  }
+
+  /// A variant of `map` that fallback to returning `null`.
+  ///
+  /// It is equivalent to doing:
+  /// ```dart
+  /// switch (sealedClass) {
+  ///   case final Subclass value:
+  ///     return ...;
+  ///   case _:
+  ///     return null;
+  /// }
+  /// ```
+
+  @optionalTypeArgs
+  TResult? mapOrNull<TResult extends Object?>({
+    TResult? Function(AliasData<T> value)? data,
+    TResult? Function(Alias<T> value)? alias,
+    TResult? Function(AliasUnresolved<T> value)? unresolved,
+  }) {
+    final _that = this;
+    switch (_that) {
+      case AliasData() when data != null:
+        return data(_that);
+      case Alias() when alias != null:
+        return alias(_that);
+      case AliasUnresolved() when unresolved != null:
+        return unresolved(_that);
+      case _:
+        return null;
+    }
+  }
+
+  /// A variant of `when` that fallback to an `orElse` callback.
+  ///
+  /// It is equivalent to doing:
+  /// ```dart
+  /// switch (sealedClass) {
+  ///   case Subclass(:final field):
+  ///     return ...;
+  ///   case _:
+  ///     return orElse();
+  /// }
+  /// ```
+
+  @optionalTypeArgs
+  TResult maybeWhen<TResult extends Object?>({
+    TResult Function(T data)? data,
+    TResult Function(String id, AliasOr<T> aliasOrValue)? alias,
+    TResult Function(String id)? unresolved,
+    required TResult orElse(),
+  }) {
+    final _that = this;
+    switch (_that) {
+      case AliasData() when data != null:
+        return data(_that.data);
+      case Alias() when alias != null:
+        return alias(_that.id, _that.aliasOrValue);
+      case AliasUnresolved() when unresolved != null:
+        return unresolved(_that.id);
+      case _:
+        return orElse();
+    }
+  }
+
+  /// A `switch`-like method, using callbacks.
+  ///
+  /// As opposed to `map`, this offers destructuring.
+  /// It is equivalent to doing:
+  /// ```dart
+  /// switch (sealedClass) {
+  ///   case Subclass(:final field):
+  ///     return ...;
+  ///   case Subclass2(:final field2):
+  ///     return ...;
+  /// }
+  /// ```
+
+  @optionalTypeArgs
+  TResult when<TResult extends Object?>({
+    required TResult Function(T data) data,
+    required TResult Function(String id, AliasOr<T> aliasOrValue) alias,
+    required TResult Function(String id) unresolved,
+  }) {
+    final _that = this;
+    switch (_that) {
+      case AliasData():
+        return data(_that.data);
+      case Alias():
+        return alias(_that.id, _that.aliasOrValue);
+      case AliasUnresolved():
+        return unresolved(_that.id);
+    }
+  }
+
+  /// A variant of `when` that fallback to returning `null`
+  ///
+  /// It is equivalent to doing:
+  /// ```dart
+  /// switch (sealedClass) {
+  ///   case Subclass(:final field):
+  ///     return ...;
+  ///   case _:
+  ///     return null;
+  /// }
+  /// ```
+
+  @optionalTypeArgs
+  TResult? whenOrNull<TResult extends Object?>({
+    TResult? Function(T data)? data,
+    TResult? Function(String id, AliasOr<T> aliasOrValue)? alias,
+    TResult? Function(String id)? unresolved,
+  }) {
+    final _that = this;
+    switch (_that) {
+      case AliasData() when data != null:
+        return data(_that.data);
+      case Alias() when alias != null:
+        return alias(_that.id, _that.aliasOrValue);
+      case AliasUnresolved() when unresolved != null:
+        return unresolved(_that.id);
+      case _:
+        return null;
+    }
+  }
 }
 
 /// @nodoc
